55418a5d796c9e5a6fee2f5a5e8704f6c7d15018
Eric S. Stroo <ericsstroo@gmail.com>
2021-02-21 10:03:16 -0800

    Create connect.sh

---
+++ connect.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $2 = '--env=PRD' ]; then
+  HOSTNAME='site@site.ssh.wpengine.net'
+  DIR='sites/site'
+fi
+
+if [ $2 = '--env=STG' ]; then
+  HOSTNAME='sitestg@sitestg.ssh.wpengine.net'
+  DIR='sites/sitestg'
+fi
+
+if [ $2 = '--env=DEV' ]; then
+  HOSTNAME='sitedev@sitedev.ssh.wpengine.net'
+  DIR='sites/sitedev'
+fi
+
+if [ $1 = '--mode=DB' ]; then
+  echo 'Backing up the database'
+  ssh -t $HOSTNAME "cd ${DIR} && wp db export db.sql"
+  echo 'Downloading the database'
+  scp $HOSTNAME:$DIR/db.sql db.sql
+  echo 'Cleaning up our messes'
+  ssh -t $HOSTNAME "cd ${DIR} && rm db.sql"
+fi
+
+if [ $1 = '--mode=FS' ]; then
+  echo 'RSYNC'
+  rsync -avz $HOSTNAME:$DIR/wp-content/uploads wp-contentc
+  
+fi
